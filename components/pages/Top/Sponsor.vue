603f5f937c23574accfbbedb5052c8beb43aa5c8
iLLviA <illvia.3594@gmail.com>
2018-12-03 16:17:28 +0900

    vue.ファイルの作成とindex.vueに組み込み

---
+++ components/pages/Top/Sponsor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    sponsor
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Sponsor"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/pages/Top/Wanted.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    wanted
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Wanted"
+    }
+</script>
+
+<style scoped>
+
+</style>
