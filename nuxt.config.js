ad58e0fd7cc53dcda198f35abcdd495a3d975bcf
yasui05821 <yasui05821@gmail.com>
2019-01-12 18:47:52 +0900

    headの中整備

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import createMeta from "./service/meta"
+
 const pkg = require('./package')
 
 module.exports = {
@@ -7,14 +9,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'PHPerKaigi 2019',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      ...createMeta(),
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', type: 'image/png', href: '/apple-touch-icon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Fugaz+One'},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.5.0/css/all.css',integrity:"sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU",crossorigin:"anonymous"}
---
+++ service/meta.mjs
@@ -0,0 +1,24 @@
+const defaultMeta = {
+  description: "PHPerKaigi（ペチパーカイギ）は、現在PHPを使用している、過去にPHPを使用していた、これからPHPを使いたいと思っているエンジニアが、技術的なノウハウを共有するためのカンファレンス（イベント）です。",
+  title: "PHPerKaigi 2019" ,
+  host: "https://phperkaigi.jp/2019/",
+  image: "/PHPerKaigi.png"
+}
+
+export default (args) => {
+  const {title,description,host,image} = {...defaultMeta,...args}
+  return [
+
+    { hid: 'DESC', name: 'description', content: description },
+    { hid: 'OG:TITLE', property: 'og:title', content: title },
+    { hid: 'OG:IMAGE', property: 'og:image', content: `${host}${image}` },
+    { hid: 'OG:DESC', property: 'og:description', content: description},
+    { hid: 'TW:CARD', name: 'twitter:card', content: 'summary_large_image' },
+    { hid: 'TW:SITE', name: 'twitter:site', content: '@phperkaigi' },
+    { hid: 'TW:CREATOR', name: 'twitter:creator', content: '@phperkaigi' },
+    { hid: 'TW:TITLE', name: 'twitter:title', content: title },
+    { hid: 'TW:DESC', name: 'twitter:description', content: description },
+    { hid: 'TW:IMAGE', name: 'twitter:image', content: `${host}${image}` },
+  ]
+
+}
